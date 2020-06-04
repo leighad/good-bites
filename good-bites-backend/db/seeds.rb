b542e049ec7d601c307941d214758706358e1c4c
leighad <cdversiontwo@gmail.com>
2020-06-03 20:17:15 -0700

    create seeds

--- good-bites-backend/db/seeds.rb
+++ good-bites-backend/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require 'securerandom'
+
+User.delete_all
+Restaurant.delete_all
+
+user_names = [
+    'Kiki',
+    'Luigi',
+    'BeeBee',
+    'Tankie',
+    'CoCo'
+]
+
+user_collection = []
+
+user_names.each do |name|
+    user_collection << User.create(user_name: name)
+end
+
+user_collection.each do |user|
+    list_size = (SecureRandom.random_number(10) + 1).floor
+
+    (1..list_size).each do |rest|
+        name = Faker::Restaurant.name
+        type = Faker::Restaurant.type
+        description = Faker::Restaurant.description
+        review = Faker::Restaurant.review
+        Restaurant.create(rest_name: name, type: type, description: description, review: review, user_id: user.id)
+    end
+end
+
