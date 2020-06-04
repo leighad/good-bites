d0d21fd431a1bbd38b31376dd030a2f65e096adc
leighad <cdversiontwo@gmail.com>
2020-06-04 16:12:12 -0700

    db:migrate update

--- good-bites-backend/db/migrate/20200604191744_rename_type_to_category.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeToCategory < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :restaurants, :type, :category 
-  end
-end
--- good-bites-backend/db/schema.rb
+++ good-bites-backend/db/schema.rb
@@ -10,17 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_191744) do
+ActiveRecord::Schema.define(version: 2020_06_04_230649) do
 
-  create_table "restaurants", force: :cascade do |t|
-    t.string "rest_name"
-    t.string "category"
-    t.string "description"
-    t.string "review"
-    t.integer "user_id", null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "group"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "review"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_restaurants_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,5 +34,5 @@ ActiveRecord::Schema.define(version: 2020_06_04_191744) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "restaurants", "users"
+  add_foreign_key "restaurants", "categories"
 end
