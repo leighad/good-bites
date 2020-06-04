86d162145594d2485c9d7f37afb3a4e76e072e1a
leighad <cdversiontwo@gmail.com>
2020-06-04 12:21:21 -0700

    rename db restaurants type to category

--- good-bites-backend/db/schema.rb
+++ good-bites-backend/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_030623) do
+ActiveRecord::Schema.define(version: 2020_06_04_191744) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "rest_name"
-    t.string "type"
+    t.string "category"
     t.string "description"
     t.string "review"
     t.integer "user_id", null: false
