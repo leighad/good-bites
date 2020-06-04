e6fd72a361675552af06530e0a64ac29aad13dfe
leighad <cdversiontwo@gmail.com>
2020-06-04 16:07:46 -0700

    generate restaurant model and overwrite previous migration

--- good-bites-backend/db/migrate/20200604230649_create_restaurants.rb
+++ good-bites-backend/db/migrate/20200604230649_create_restaurants.rb
@@ -1,11 +1,10 @@
 class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
-      t.string :rest_name
-      t.string :type
+      t.string :name
       t.string :description
       t.string :review
-      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
