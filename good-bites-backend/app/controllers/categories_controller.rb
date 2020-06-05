d5c052689f8e030fa0bf7d8a58e036b097886897
leighad <cdversiontwo@gmail.com>
2020-06-04 19:59:40 -0700

    create update and destroy actions

--- good-bites-backend/app/controllers/categories_controller.rb
+++ good-bites-backend/app/controllers/categories_controller.rb
@@ -8,4 +8,27 @@ class CategoriesController < ApplicationController
         category = Category.find_by_id(params[:id])
         render json: category, include: [:restaurants]
     end
+
+    def create
+        category = Category.create(category_params)
+        render json: category, include: [:restaurants]
+    end
+
+    def update
+        category = Category.find_by_id(params[:id])
+        category.update(category_params)
+        render json: category, include: [:restaurants]
+    end
+
+    def destroy
+        category = Category.find_by_id(params[:id])
+        category.delete
+        render json: {category_id: category.id}
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:group)
+    end
 end
