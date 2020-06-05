a1ccb77cdf90a27a482d8af5d18ad71d1622bf58
leighad <cdversiontwo@gmail.com>
2020-06-04 17:23:54 -0700

    drop users table

--- good-bites-backend/db/schema.rb
+++ good-bites-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_230649) do
+ActiveRecord::Schema.define(version: 2020_06_05_002138) do
 
   create_table "categories", force: :cascade do |t|
     t.string "group"
@@ -26,12 +26,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_230649) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_restaurants_on_category_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "restaurants", "categories"
--- good-bites-backend/db/seeds.rb
+++ good-bites-backend/db/seeds.rb
@@ -6,35 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'faker'
-# require 'securerandom'
+require 'faker'
+require 'securerandom'
 
-# User.destroy_all 
-# Restaurant.destroy_all 
+Category.destroy_all 
+Restaurant.destroy_all 
 
-# user_names = [
-#     'Kiki',
-#     'Luigi',
-#     'BeeBee',
-#     'Tankie',
-#     'CoCo'
-# ]
+category_names = []
+30.times do 
+    group = Faker::Restaurant.type
+    category = Category.create(group: group)
+    category_names << category
+end
+category_names
 
-# user_collection = []
+group_names = category_names.map {|cat| cat.group}.uniq
 
-# user_names.each do |name|
-#     user_collection << User.create(user_name: name)
-# end
+
+restaurant_list = []
+30.times do 
+    name = Faker::Restaurant.name
+    description = Faker::Restaurant.description
+    review = Faker::Restaurant.review
+    Restaurant.create(rest_name: name, type: type, description: description, review: review, user_id: user.id)
+    group = Faker::Restaurant.type
+    category = Category.create(group: group)
+    category_names << category
+end
+category_names
+
 
 # user_collection.each do |user|
 #     list_size = (SecureRandom.random_number(10) + 1).floor
 
 #     (1..list_size).each do |rest|
 #         name = Faker::Restaurant.name
-#         type = Faker::Restaurant.type
+#         # type = Faker::Restaurant.type
 #         description = Faker::Restaurant.description
 #         review = Faker::Restaurant.review
 #         Restaurant.create(rest_name: name, type: type, description: description, review: review, user_id: user.id)
 #     end
+# end
+
+# user_names.each do |name|
+#     user_collection << User.create(user_name: name)
 # end
 
