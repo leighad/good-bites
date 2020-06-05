286648f5310c0ec33b98fddae25c47e9bfb6f0f9
leighad <cdversiontwo@gmail.com>
2020-06-04 18:54:40 -0700

    add loadCategories and renderCategory

--- good-bites-frontend/src/index.js
+++ good-bites-frontend/src/index.js
@@ -4,3 +4,34 @@ const RESTAURANTS_URL = `${BASE_URL}/restaurants`
 
 const main = document.querySelector("main")
 
+document.addEventListener("DOMContentLoaded", () => loadCategories())
+
+const loadCategories = () => {
+    fetch(CATEGORIES_URL)
+    .then(res => res.json())
+    .then(json => {
+        json.forEach(category => renderCategory(category))
+    })
+}
+
+const renderCategory = (categoryHash) => {
+    const div = document.createElement("div")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    const ul = document.createElement("ul")
+
+    div.setAttribute("class", "list")
+    div.setAttribute("data-id", categoryHash.id)
+
+    p.innerHTML = categoryHash.name
+
+    button.setAttribute("data-category-id", categoryHash.id)
+    button.innerHTML = "Add Restaurant"
+    button.addEventListener("click", createRestaurant)
+
+    div.appendChild(p)
+    div.appendChild(button)
+    div.appendChild(ul)
+    main.appendChild(div)
+
+}
