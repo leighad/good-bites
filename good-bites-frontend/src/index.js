0f3f6f67d9a935063c6abd7e8ce50d902a91fbb4
leighad <cdversiontwo@gmail.com>
2020-06-09 13:02:41 -0700

    update mountFormListenerand add clearForm

--- good-bites-frontend/src/index.js
+++ good-bites-frontend/src/index.js
@@ -13,32 +13,32 @@ const loadCategories = () => {
     fetch(CATEGORIES_URL)
     .then(res => res.json())
     .then(json => {
-        json.forEach(category => renderCategory(category))
+        json.forEach(category => getCategoryData(category))
     })
 }
 
-const renderCategory = (categoryHash) => {
-    const div = document.createElement("div")
-    const span = document.createElement("span")
-    const p = document.createElement("p")
-    const button = document.createElement("button")
-    const ul = document.createElement("ul")
+// const renderCategory = (categoryHash) => {
+//     const div = document.createElement("div")
+//     const span = document.createElement("span")
+//     const p = document.createElement("p")
+//     const button = document.createElement("button")
+//     const ul = document.createElement("ul")
 
-    div.setAttribute("class", "list")
-    div.setAttribute("data-id", categoryHash.id)
+//     div.setAttribute("class", "list")
+//     div.setAttribute("data-id", categoryHash.id)
 
-    p.innerHTML = categoryHash.name
+//     p.innerHTML = categoryHash.name
 
-    button.setAttribute("data-category-id", categoryHash.id)
-    button.innerHTML = "Add Restaurant"
-    // button.addEventListener("click", createRestaurant)
+//     button.setAttribute("data-category-id", categoryHash.id)
+//     button.innerHTML = "Add Restaurant"
+//     // button.addEventListener("click", createRestaurant)
 
-    div.appendChild(p)
-    div.appendChild(button)
-    div.appendChild(ul)
-    main.appendChild(div)
+//     div.appendChild(p)
+//     div.appendChild(button)
+//     div.appendChild(ul)
+//     main.appendChild(div)
 
-}
+// }
 
 function getCategoryData(event) {
     return {
@@ -46,10 +46,35 @@ function getCategoryData(event) {
     }
 }
 
+const htmlifyCategory = function(cat) {
+    return (`
+        <div class="card">
+            <div class="card-content">
+                <span class="card-group">${cat.group}</span>
+                <!-- <p>Category</p> -->
+            </div>
+        </div>
+    `
+    )
+}
+
+function clearForm(event) {
+    event.target.querySelector("#group").value = ""
+}
+
+function renderCategory(category) {
+    const categoryList = document.querySelector("#category-container")
+    categoryList.innerHTML += category
+
+}
+
 function mountFormListener() {
     const categoryForm = document.getElementById("category-form-container")
     categoryForm.addEventListener("submit", function(event) {
-        const categoryText = getCategoryData(event)
-        debugger 
+        event.preventDefault()
+        const categoryObject = getCategoryData(event)
+        const htmlCat = htmlifyCategory(categoryObject)
+        renderCategory(htmlCat)
+        clearForm(event)
     })
 }
