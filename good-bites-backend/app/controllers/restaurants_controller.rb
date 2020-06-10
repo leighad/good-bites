87730bf959014a14469da0c4bd293cb4e3576234
leighad <cdversiontwo@gmail.com>
2020-06-10 14:56:11 -0700

    add create and private methods

--- good-bites-backend/app/controllers/restaurants_controller.rb
+++ good-bites-backend/app/controllers/restaurants_controller.rb
@@ -1,11 +1,32 @@
 class RestaurantsController < ApplicationController
+    before_action :set_restaurant, only: [:show, :update, :destroy]
+
     def index 
         restaurants = Restaurant.all
         render json: restaurants
     end
 
     def show
-        restaurant = Restaurant.find_by_id(params[:id])
+        set_restaurant
         render json: restaurant
+    end
+
+    def create
+        restaurant = Restaurant.new(restaurant_params)
+        if restaurant.save
+            render json: restaurant
+        else
+            render json: { errors: restaurant.errors.full_messages }
+        end
+    end
+
+    private
+
+    def set_restaurant
+        restaurant = Restaurant.find_by_id(params[:id])
+    end
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :description, :review, :category_id)
     end
 end
