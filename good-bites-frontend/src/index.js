1f12981483aa01bd9020734d08de9828c9a3d4aa
leighad <cdversiontwo@gmail.com>
2020-06-10 17:47:05 -0700

    add and modify style to restaurants list

--- good-bites-frontend/src/index.js
+++ good-bites-frontend/src/index.js
@@ -6,12 +6,19 @@ const main = document.querySelector("main")
 
 document.addEventListener("DOMContentLoaded", function() {
     loadCategories()
+    loadRestaurants()
     mountFormListener()
 })
 
 function addCategoriesToDOM(categories) {
     categories.forEach(function(cat) {
         renderCategory(htmlifyCategory(cat))
+    })
+}
+
+function addRestaurantsToDOM(restaurants) {
+    restaurants.forEach(function(rest) {
+        renderRestaurant(htmlifyRestaurant(rest))
     })
 }
 
@@ -26,28 +33,47 @@ const loadCategories = () => {
     })
 }
 
-// const renderCategory = (categoryHash) => {
-//     const div = document.createElement("div")
-//     const span = document.createElement("span")
-//     const p = document.createElement("p")
-//     const button = document.createElement("button")
-//     const ul = document.createElement("ul")
+const loadRestaurants = () => {
+    fetch(RESTAURANTS_URL)
+    .then(res => res.json())
+    .then(json => {
+        json.forEach(restaurant => renderRestaurant(restaurant))
+    })
+}
 
-//     div.setAttribute("class", "list")
-//     div.setAttribute("data-id", categoryHash.id)
+const renderRestaurant = (restaurantHash) => {
+    const div = document.createElement("div")
+    const h3 = document.createElement("h3")
+    const h5 = document.createElement("h5")
+    const h6 = document.createElement("h6")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    // const ul = document.createElement("ul")
+    // const li = document.createElement("li")
 
-//     p.innerHTML = categoryHash.name
 
-//     button.setAttribute("data-category-id", categoryHash.id)
-//     button.innerHTML = "Add Restaurant"
-//     // button.addEventListener("click", createRestaurant)
+    div.setAttribute("class", "list")
+    div.setAttribute("data-id", restaurantHash.id)
 
-//     div.appendChild(p)
-//     div.appendChild(button)
-//     div.appendChild(ul)
-//     main.appendChild(div)
+    h3.innerHTML = restaurantHash.name
+    h5.innerHTML = restaurantHash.description
+    h6.innerHTML = restaurantHash.review
+    p.innerHTML = restaurantHash.category.group
 
-// }
+    button.setAttribute("data-restaurant-id", restaurantHash.id)
+    button.innerHTML = "Save Restaurant"
+    // button.addEventListener("click", saveRestaurant)
+
+    div.appendChild(h3)
+    div.appendChild(h5)
+    div.appendChild(h6)
+    div.appendChild(p)
+    div.appendChild(button)
+    // div.appendChild(ul)
+    // div.appendChild(li)
+    main.appendChild(div)
+
+}
 
 function getCategoryData(event) {
     return {
@@ -68,6 +94,20 @@ const htmlifyCategory = function(cat) {
     )
 }
 
+// const htmlifyRestaurant = function(rest) {
+//     return (`
+//         <div class="list">
+//             <div class="list-content">
+//                 <span class="list-name">${rest.name}</span>
+//                 <span class="list-name">${rest.description}</span>
+//                 <span class="list-name">${rest.review}</span>
+//                 <span class="list-name">${rest.category.group}</span>
+//             </div>
+//         </div>
+//     `
+//     )
+// }
+
 function clearForm(event) {
     event.target.querySelector("#group").value = ""
 }
@@ -77,6 +117,12 @@ function renderCategory(category) {
     categoryList.innerHTML += category
 
 }
+
+// function renderRestaurant(restaurant) {
+//     const restaurantList = document.querySelector("#restaurant-container")
+//     restaurantList.innerHTML += restaurant
+
+// }
 
 function mountFormListener() {
     const categoryForm = document.getElementById("category-form-container")
