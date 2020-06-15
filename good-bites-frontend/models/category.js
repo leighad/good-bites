f5f26d6a152153e1456d96892677b99b94cd47d9
leighad <cdversiontwo@gmail.com>
2020-06-15 14:58:38 -0700

    refactor category for oo

--- good-bites-frontend/models/category.js
+++ good-bites-frontend/models/category.js
@@ -5,33 +5,100 @@ class Category {
         this.id = id
         this.group = group
         Category.all.push(this)
-
     }
 
-    htmlifyCategory() {
-        return (`
-                <span class="card-group">${this.group}</span>
-                <br>
-        `)
+    // htmlifyCategory() {
+    //     return (`
+    //             <span class="card-group">${this.group}</span>
+    //             <br>
+    //     `)
+    // }
+
+    static renderCategory(cat) {
+        const categoryList = document.querySelector("#category-list-container")
+        categoryList.innerHTML += cat.group
     }
 
-    renderCategory() {
-        categoryList.innerHTML += this.htmlifyCategory()
-
-    }
-
-    static renderCategories() {
+    static addCategoriesToDOM() {
+        const categoryList = document.querySelector("#category-list-container")
+        const selectCategory = document.querySelector("#category")
+        
         categoryList.innerHTML = ""
-        Category.all.forEach(cat => cat.renderCategory())
+        selectCategory.innerHTML = ""
+    
+        Category.all.forEach(function(cat) {
+            // renderCategory(htmlifyCategory(cat))
+            // const categoryList = document.querySelector("#category-list-container")
+            categoryList.innerHTML += `${cat.group}<br>` 
+
+            selectCategory.innerHTML += `<option value=${cat.id}>${cat.group}</option>`
+        })
     }
+
+    // static renderCategories() {
+    //     const categoryList = document.querySelector("#category-list-container")
+    //     categoryList.innerHTML = ""
+    //     Category.all.forEach(cat => cat.renderCategory())
+    // }
 
     static loadCategories() {
         //this will send request and create all categories
-        API.get() //this will create all categories
+        API.get()
         .then(cats => {
             cats.forEach(cat => new Category(cat))
-            Category.renderCategories() //this will render all categories
+            // Category.renderCategories() //this will render all categories
         })
+        .then(data => {
+            Category.addCategoriesToDOM(data)
+        })
+    }
+
+    // getCategoryData(event) {
+    //     return {
+    //         group: event.target.querySelector("#group").value
+    //     }
+    // }
+
+    // clearCatForm(event) {
+    //     this.event.target.querySelector("#group").value = ""
+    // }
+
+    static categoryFormListener() {
+        const categoryForm = document.getElementById("category-form-container")
+        categoryForm.addEventListener("submit", function(event) {
+            event.preventDefault()
+            const categoryObject = { group: event.target.querySelector("#group").value}
+            event.target.querySelector("#group").value = ""
+    
+            fetch(CATEGORIES_URL, {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({category:categoryObject}) 
+                // body data type must match "Content-Type" header
+            })
+            .then(res => res.json())
+            // .then((json) => {
+                // const categoryList = document.querySelector("#category-list-container")
+                // categoryList.innerHTML = ""
+                // Category.loadCategories(json)
+
+                // Category.renderCategory(json)
+                // Category.addCategoriesToDOM()
+
+                // Category.loadCategories()
+                // clearCatForm(event)
+            // }) 
+            .then((json) => {
+                const categoryList = document.querySelector("#category-list-container")
+
+                categoryList.innerHTML += json.group 
+                // Category.loadCategories()
+            })
+
+        })
+
     }
 
 }
--- good-bites-frontend/src/index.js
+++ good-bites-frontend/src/index.js
@@ -6,28 +6,31 @@ const RESTAURANTS_URL = `${BASE_URL}/restaurants`
 const main = document.querySelector("main")
 
 document.addEventListener("DOMContentLoaded", function() {
-    loadCategories()
-    // Category.loadCategories()
+    Category.loadCategories()
+    
     // loadRestaurants()
     restaurantsClickListener()
     // categoriesClickListener()
-    categoryFormListener()
+
+    Category.categoryFormListener()
+    // categoryFormListener()
     restaurantFormListener()
 })
 
-const categoryList = document.querySelector("#category-list-container")
-const selectCategory = document.querySelector("#category")
+// const categoryList = document.querySelector("#category-list-container")
+// const selectCategory = document.querySelector("#category")
 const restaurantList = document.querySelector("#restaurant-container")
 
-function addCategoriesToDOM(categories) {
-    categoryList.innerHTML = ""
-    selectCategory.innerHTML = ""
+//ORIGINAL CODE******
+// function addCategoriesToDOM(categories) {
+//     categoryList.innerHTML = ""
+//     selectCategory.innerHTML = ""
 
-    categories.forEach(function(cat) {
-        renderCategory(htmlifyCategory(cat))
-        selectCategory.innerHTML += `<option value=${cat.id}>${cat.group}</option>`
-    })
-}
+//     categories.forEach(function(cat) {
+//         renderCategory(htmlifyCategory(cat))
+//         selectCategory.innerHTML += `<option value=${cat.id}>${cat.group}</option>`
+//     })
+// }
 
 function addRestaurantsToDOM(restaurants) {
     restaurantList.innerHTML = ""
@@ -42,20 +45,20 @@ function addRestaurantsToDOM(restaurants) {
         selectName.innerHTML += `<option value=${rest.id}>${rest.name}</option>`
         selectDescription.innerHTML += `<option value=${rest.id}>${rest.description}</option>`
         selectReview.innerHTML += `<option value=${rest.id}>${rest.review}</option>`
-        selectCategory.innerHTML += `<option value=${rest.category_id}>${rest.category.group}</option>`
+        selectCategory.innerHTML += `<option value=${rest.category.id}>${rest.category.group}</option>`
     })
 }
-
-const loadCategories = () => {
-    fetch(CATEGORIES_URL)
-    .then(res => res.json())
-    // .then(json => {
-    //     json.forEach(category => getCategoryData(category))
-    // })
-    .then(data => {
-        addCategoriesToDOM(data)
-    })
-}
+//ORIGINAL CODE******
+// const loadCategories = () => {
+//     fetch(CATEGORIES_URL)
+//     .then(res => res.json())
+//     // .then(json => {
+//     //     json.forEach(category => getCategoryData(category))
+//     // })
+//     .then(data => {
+//         addCategoriesToDOM(data)
+//     })
+// }
 
 // NOT CURRENTLY WORKING
 // function categoriesClickListener() {
@@ -95,9 +98,10 @@ const loadRestaurants = () => {
 //     })
 // }
 
-function renderCategory(category) {
-    categoryList.innerHTML += category
-}
+//ORIGINAL CODE******
+// function renderCategory(category) {
+//     categoryList.innerHTML += category
+// }
 
 const renderRestaurant = (restaurantHash) => {
     const div = document.createElement("div")
@@ -129,18 +133,20 @@ const renderRestaurant = (restaurantHash) => {
     main.appendChild(div)
 }
 
-function getCategoryData(event) {
-    return {
-        group: event.target.querySelector("#group").value
-    }
-}
+//ORIGINAL CODE******
+// function getCategoryData(event) {
+//     return {
+//         group: event.target.querySelector("#group").value
+//     }
+// }
 
-const htmlifyCategory = function(cat) {
-    return (`
-            <span class="card-group">${cat.group}</span>
-            <br>
-    `)
-}
+//ORIGINAL CODE******
+// const htmlifyCategory = function(cat) {
+//     return (`
+//             <span class="card-group">${cat.group}</span>
+//             <br>
+//     `)
+// }
 
 function getRestaurantData(event) { 
     return {
@@ -165,9 +171,11 @@ function getRestaurantData(event) {
 //     )
 // }
 
-function clearCatForm(event) {
-    event.target.querySelector("#group").value = ""
-}
+
+//ORIGINAL CODE******
+// function clearCatForm(event) {
+//     event.target.querySelector("#group").value = ""
+// }
 
 function clearRestForm(event) {
     event.target.querySelector("#name").value = ""
@@ -176,31 +184,30 @@ function clearRestForm(event) {
     event.target.querySelector("#category").value = "4"
 }
 
-function categoryFormListener() {
-    const categoryForm = document.getElementById("category-form-container")
-    categoryForm.addEventListener("submit", function(event) {
-        event.preventDefault()
-        const categoryObject = getCategoryData(event)
+//ORIGINAL CODE******
+// function categoryFormListener() {
+//     const categoryForm = document.getElementById("category-form-container")
+//     categoryForm.addEventListener("submit", function(event) {
+//         event.preventDefault()
+//         const categoryObject = getCategoryData(event)
 
-        fetch(CATEGORIES_URL, {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({category:categoryObject}) 
-            // body data type must match "Content-Type" header
-        })
-        .then(res => res.json())
-        .then((data) => {
-            // const htmlCat = htmlifyCategory(data)
-            // renderCategory(htmlCat)
-            loadCategories()
-            clearCatForm(event)        
-        })
-
-
-    })
-}
+//         fetch(CATEGORIES_URL, {
+//             method: 'POST', 
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({category:categoryObject}) 
+//             // body data type must match "Content-Type" header
+//         })
+//         .then(res => res.json())
+//         .then((data) => {
+//             // const htmlCat = htmlifyCategory(data)
+//             // renderCategory(htmlCat)
+//             loadCategories()
+//             clearCatForm(event)        
+//         })
+//     })
+// }
 
 function restaurantFormListener() {
     const restaurantForm = document.getElementById("restaurant-form-container")
@@ -220,9 +227,10 @@ function restaurantFormListener() {
         .then((data) => {
             // const htmlRest = htmlifyRestaurant(data)
             // renderRestaurant(htmlRest)
+
             loadRestaurants()
             clearRestForm(event)        
         })
-
     })
+
 }
