eca18535336416887969db0e5eff3d11d2c72fdb
leighad <cdversiontwo@gmail.com>
2020-06-15 21:39:08 -0700

    add script tag for restaurant and create render/add to DOM

--- good-bites-frontend/models/restaurant.js
+++ good-bites-frontend/models/restaurant.js
@@ -10,4 +10,51 @@ class Restaurant {
         Restaurant.all.push(this)
     }
 
+    static renderRestaurant(restaurantHash) {
+        const div = document.createElement("div")
+        const h3 = document.createElement("h3")
+        const h5 = document.createElement("h5")
+        const h6 = document.createElement("h6")
+        const p = document.createElement("p")
+        const button = document.createElement("button")
+    
+        // div.setAttribute("id", "restaurant-container")
+        div.setAttribute("data-id", restaurantHash.id)
+    
+        h3.innerHTML = restaurantHash.name
+        h5.innerHTML = "Description: <br>" + restaurantHash.description
+        h6.innerHTML = "Review: <br>" + restaurantHash.review
+        p.innerHTML = restaurantHash.category.group
+    
+        button.setAttribute("data-restaurant-id", restaurantHash.id)
+        button.innerHTML = "Save Restaurant"
+        // button.addEventListener("click", saveRestaurant)
+    
+        div.appendChild(h3)
+        div.appendChild(h5)
+        div.appendChild(h6)
+        div.appendChild(p)
+        div.appendChild(button)
+        restaurantList.appendChild(div)
+    
+        main.appendChild(div)
+    }
+
+    static addRestaurantsToDOM(restaurants) {
+        restaurantList.innerHTML = ""
+        const selectName = document.querySelector("#name").value 
+        const selectDescription = document.querySelector("#description").value 
+        const selectReview = document.querySelector("#review").value 
+        const selectCategory = document.querySelector("#category").value 
+    
+        restaurants.forEach(function(rest) {
+            // renderRestaurant(htmlifyRestaurant(rest))
+            renderRestaurant(rest)
+            selectName.innerHTML += `<option value=${rest.id}>${rest.name}</option>`
+            selectDescription.innerHTML += `<option value=${rest.id}>${rest.description}</option>`
+            selectReview.innerHTML += `<option value=${rest.id}>${rest.review}</option>`
+            selectCategory.innerHTML += `<option value=${rest.category.id}>${rest.category.group}</option>`
+        })
+    }
+
 }
--- good-bites-frontend/src/index.js
+++ good-bites-frontend/src/index.js
@@ -32,22 +32,24 @@ const restaurantList = document.querySelector("#restaurant-container")
 //     })
 // }
 
-function addRestaurantsToDOM(restaurants) {
-    restaurantList.innerHTML = ""
-    const selectName = document.querySelector("#name").value 
-    const selectDescription = document.querySelector("#description").value 
-    const selectReview = document.querySelector("#review").value 
-    const selectCategory = document.querySelector("#category").value 
+//ORIGINAL CODE
+// function addRestaurantsToDOM(restaurants) {
+//     restaurantList.innerHTML = ""
+//     const selectName = document.querySelector("#name").value 
+//     const selectDescription = document.querySelector("#description").value 
+//     const selectReview = document.querySelector("#review").value 
+//     const selectCategory = document.querySelector("#category").value 
 
-    restaurants.forEach(function(rest) {
-        // renderRestaurant(htmlifyRestaurant(rest))
-        renderRestaurant(rest)
-        selectName.innerHTML += `<option value=${rest.id}>${rest.name}</option>`
-        selectDescription.innerHTML += `<option value=${rest.id}>${rest.description}</option>`
-        selectReview.innerHTML += `<option value=${rest.id}>${rest.review}</option>`
-        selectCategory.innerHTML += `<option value=${rest.category.id}>${rest.category.group}</option>`
-    })
-}
+//     restaurants.forEach(function(rest) {
+//         // renderRestaurant(htmlifyRestaurant(rest))
+//         renderRestaurant(rest)
+//         selectName.innerHTML += `<option value=${rest.id}>${rest.name}</option>`
+//         selectDescription.innerHTML += `<option value=${rest.id}>${rest.description}</option>`
+//         selectReview.innerHTML += `<option value=${rest.id}>${rest.review}</option>`
+//         selectCategory.innerHTML += `<option value=${rest.category.id}>${rest.category.group}</option>`
+//     })
+// }
+
 //ORIGINAL CODE******
 // const loadCategories = () => {
 //     fetch(CATEGORIES_URL)
@@ -103,35 +105,36 @@ const loadRestaurants = () => {
 //     categoryList.innerHTML += category
 // }
 
-const renderRestaurant = (restaurantHash) => {
-    const div = document.createElement("div")
-    const h3 = document.createElement("h3")
-    const h5 = document.createElement("h5")
-    const h6 = document.createElement("h6")
-    const p = document.createElement("p")
-    const button = document.createElement("button")
+//ORIGINAL CODE
+// const renderRestaurant = (restaurantHash) => {
+//     const div = document.createElement("div")
+//     const h3 = document.createElement("h3")
+//     const h5 = document.createElement("h5")
+//     const h6 = document.createElement("h6")
+//     const p = document.createElement("p")
+//     const button = document.createElement("button")
 
-    // div.setAttribute("id", "restaurant-container")
-    div.setAttribute("data-id", restaurantHash.id)
+//     // div.setAttribute("id", "restaurant-container")
+//     div.setAttribute("data-id", restaurantHash.id)
 
-    h3.innerHTML = restaurantHash.name
-    h5.innerHTML = "Description: <br>" + restaurantHash.description
-    h6.innerHTML = "Review: <br>" + restaurantHash.review
-    p.innerHTML = restaurantHash.category.group
+//     h3.innerHTML = restaurantHash.name
+//     h5.innerHTML = "Description: <br>" + restaurantHash.description
+//     h6.innerHTML = "Review: <br>" + restaurantHash.review
+//     p.innerHTML = restaurantHash.category.group
 
-    button.setAttribute("data-restaurant-id", restaurantHash.id)
-    button.innerHTML = "Save Restaurant"
-    // button.addEventListener("click", saveRestaurant)
+//     button.setAttribute("data-restaurant-id", restaurantHash.id)
+//     button.innerHTML = "Save Restaurant"
+//     // button.addEventListener("click", saveRestaurant)
 
-    div.appendChild(h3)
-    div.appendChild(h5)
-    div.appendChild(h6)
-    div.appendChild(p)
-    div.appendChild(button)
-    restaurantList.appendChild(div)
+//     div.appendChild(h3)
+//     div.appendChild(h5)
+//     div.appendChild(h6)
+//     div.appendChild(p)
+//     div.appendChild(button)
+//     restaurantList.appendChild(div)
 
-    main.appendChild(div)
-}
+//     main.appendChild(div)
+// }
 
 //ORIGINAL CODE******
 // function getCategoryData(event) {
